9a229337cabc5ceee54ad6a788f9f70b5f5d4a62
GiuseppeTumminello <Giuseppe.Tumminello@acoustic.co>
2022-06-07 15:39:13 +0200

    Adjusted the layout

--- src/main/java/org/example/SalaryCalculator.java
+++ src/main/java/org/example/SalaryCalculator.java
@@ -30,50 +30,71 @@ public class SalaryCalculator {
 
 
     public BigDecimal getZusTotal() {
-        return getZusDisability().add(getZusPension()).add(getZusSickness()).setScale(2, RoundingMode.HALF_EVEN);
+        return getZusDisability()
+                .add(getZusPension())
+                .add(getZusSickness())
+                .setScale(2, RoundingMode.HALF_EVEN);
 
     }
 
     public BigDecimal getHealthNfzTotal() {
-        return this.gross.subtract(getZusTotal()).multiply(BigDecimal.valueOf(healthNFZRate)).divide(BigDecimal.valueOf(100.0d), RoundingMode.HALF_UP).setScale(2, RoundingMode.HALF_EVEN);
+        return this.gross.subtract(getZusTotal())
+                .multiply(BigDecimal.valueOf(healthNFZRate))
+                .divide(BigDecimal.valueOf(100.0d), RoundingMode.HALF_UP)
+                .setScale(2, RoundingMode.HALF_EVEN);
 
     }
 
     public BigDecimal getAdvancePaymentTotal() {
         if (this.getAnnualSum().compareTo(BigDecimal.valueOf(120_000.00)) < 0) {
             this.taxRate = 17;
-            return (this.gross.subtract((getHealthNfzTotal().add(getZusTotal())))).multiply(BigDecimal.valueOf(8.32d)).divide(BigDecimal.valueOf(100.0d), RoundingMode.HALF_UP).setScale(2, RoundingMode.HALF_EVEN);
+            return (this.gross.subtract((getHealthNfzTotal().add(getZusTotal()))))
+                    .multiply(BigDecimal.valueOf(8.32d))
+                    .divide(BigDecimal.valueOf(100.0d), RoundingMode.HALF_UP)
+                    .setScale(2, RoundingMode.HALF_EVEN);
 
         } else {
             this.taxRate = 32;
-            return (this.gross.subtract((getHealthNfzTotal().add(getZusTotal())))).multiply(BigDecimal.valueOf(14.32d)).divide(BigDecimal.valueOf(100.0d), RoundingMode.HALF_UP).setScale(2, RoundingMode.HALF_EVEN);
+            return (this.gross.subtract((getHealthNfzTotal().add(getZusTotal()))))
+                    .multiply(BigDecimal.valueOf(14.32d)).divide(BigDecimal.valueOf(100.0d), RoundingMode.HALF_UP)
+                    .setScale(2, RoundingMode.HALF_EVEN);
         }
     }
 
 
     public BigDecimal getNet() {
-        return this.gross.subtract(getZusTotal().add(getHealthNfzTotal()).add(getAdvancePaymentTotal())).setScale(2, RoundingMode.HALF_EVEN);
+        return this.gross.subtract(getZusTotal()
+                .add(getHealthNfzTotal())
+                .add(getAdvancePaymentTotal()))
+                .setScale(2, RoundingMode.HALF_EVEN);
 
     }
 
     public BigDecimal getZusPension() {
-        return this.gross.multiply(BigDecimal.valueOf(zusPensionRate)).divide(BigDecimal.valueOf(100.0d), RoundingMode.HALF_UP).setScale(2, RoundingMode.HALF_EVEN);
+        return this.gross.multiply(BigDecimal.valueOf(zusPensionRate))
+                .divide(BigDecimal.valueOf(100.0d), RoundingMode.HALF_UP)
+                .setScale(2, RoundingMode.HALF_EVEN);
     }
 
     public BigDecimal getZusDisability() {
-        return this.gross.multiply(BigDecimal.valueOf(zusDisabilityRate)).divide(BigDecimal.valueOf(100.0d), RoundingMode.HALF_UP).setScale(2, RoundingMode.HALF_EVEN);
+        return this.gross.multiply(BigDecimal.valueOf(zusDisabilityRate))
+                .divide(BigDecimal.valueOf(100.0d), RoundingMode.HALF_UP)
+                .setScale(2, RoundingMode.HALF_EVEN);
 
 
     }
 
     public BigDecimal getZusSickness() {
-        return this.gross.multiply(BigDecimal.valueOf(zusSicknessRate)).divide(BigDecimal.valueOf(100.0d), RoundingMode.HALF_UP).setScale(2, RoundingMode.HALF_EVEN);
+        return this.gross.multiply(BigDecimal.valueOf(zusSicknessRate))
+                .divide(BigDecimal.valueOf(100.0d), RoundingMode.HALF_UP)
+                .setScale(2, RoundingMode.HALF_EVEN);
 
     }
 
 
     public BigDecimal getAnnualSum() {
-        return this.gross.multiply(BigDecimal.valueOf(12.0d)).setScale(2, RoundingMode.HALF_EVEN);
+        return this.gross.multiply(BigDecimal.valueOf(12.0d))
+                .setScale(2, RoundingMode.HALF_EVEN);
     }
 
 
