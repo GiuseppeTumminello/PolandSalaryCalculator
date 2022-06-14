0284a8830c917e36f945bbf7057dcda36c206d7a
GiuseppeTumminello <Giuseppe.Tumminello@acoustic.co>
2022-06-14 16:38:18 +0200

    cleaned up the pom file

--- src/main/java/com/acoustic/salarycalculator/jobscategories/JobsCategory.java
+++ src/main/java/com/acoustic/salarycalculator/jobscategories/JobsCategory.java
@@ -12,7 +12,8 @@ import lombok.Getter;
 @Getter
 public enum JobsCategory {
 
-    IT(1, "It", new ArrayList<>(List.of("DevOps Engineer",
+    IT(1, "It", new ArrayList<>(List.of(
+            "DevOps Engineer",
             "Software Developer",
             "Software Engineer",
             "Cloud System Engineer",
@@ -23,7 +24,8 @@ public enum JobsCategory {
             "Database Administrator",
             "System Architect",
             "Web Administrator"))),
-    FINANCE(2, "Finance", new ArrayList<>(List.of("Fund Accountant",
+    FINANCE(2, "Finance", new ArrayList<>(List.of(
+            "Fund Accountant",
             "Depositary",
             "Accountant",
             "Tax Analyst",
@@ -32,14 +34,16 @@ public enum JobsCategory {
             "Business Analyst",
             "Billing Administrator",
             "Financial Controller"))),
-    ENGINEER(3, "Engineer", new ArrayList<>(List.of("Mechanic Engineer",
+    ENGINEER(3, "Engineer", new ArrayList<>(List.of(
+            "Mechanic Engineer",
             "Civil Engineer",
             "Project Engineer",
             "Test Engineer",
             "Sales Engineer",
             "R&D Engineer",
             "Thermal Engineer "))),
-    RESTAURANT(4, "Restaurant", new ArrayList<>(List.of("Executive Chef",
+    RESTAURANT(4, "Restaurant", new ArrayList<>(List.of(
+            "Executive Chef",
             "Assistant Manager",
             "General Manager",
             "Sous Chef",
@@ -50,7 +54,8 @@ public enum JobsCategory {
             "Cashier",
             "Dishwasher",
             "Waitress"))),
-    AIRLINE(5, "Airline", new ArrayList<>(List.of("Air Crew",
+    AIRLINE(5, "Airline", new ArrayList<>(List.of(
+            "Air Crew",
             "Airline Captain",
             "Airline Pilot",
             "Airport Manager",
